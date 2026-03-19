14b08e0e53c8e18b4800bcb039fed539356aadd3
yutani.prodigy <prodigyasml.rtx@gmail.com>
2026-03-19 03:18:45 -0500

    fix wallet styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@solana/wallet-adapter-react-ui/styles.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
